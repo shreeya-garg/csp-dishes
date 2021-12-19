b04cdf9623140e8bf6339c86d0e2811f719b3a7c
shreeya-garg <94569083+shreeya-garg@users.noreply.github.com>
2021-12-19 12:44:52 -0800

    Added reviews page. and changes to home page
    New rollovever functionality and impage on homepage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
-//function that displays the character that had been inputed.
+function changeImage(x) {
+  var image=document.getElementById("myimg");
+  if(x.id=="link_italian"){
+	image.src = "./../images/italiandessertsbackground.jpg";
+  }
+  if(x.id=="link_mexican"){
+	image.src = "./../images/mexicandessertsbackground.jpg";
+  }
+  if(x.id=="link_thai"){
+	image.src = "./../images/thaidessertsbackground.jpg";
+  }
+  if(x.id=="link_indian"){
+	image.src = "./../images/indiandessertsbackground.jpg";
+  }
+  if(x.id=="link_medi"){
+	image.src = "./../images/greek-desserts.jpg";
+  }
+
+}
---
+++ src/scripts/reviews.js
@@ -0,0 +1,77 @@
+function validateName() {
+
+      var name = document.getElementById('contact-name').value;
+      if(name.length == 0) {
+        producePrompt('Name is required<br>', 'name-error' , 'red')
+        return false;
+
+    }
+
+    if (!name.match(/^[A-Za-z\s]*$/)) {
+
+        producePrompt('Invalid characters in Name field, only letters and space allowed<br>','name-error', 'red');
+        return false;
+
+    }
+	producePrompt('','name-error','red');
+	return true;
+
+}
+
+function validateEmail () {
+	var email = document.getElementById('contact-email').value;
+
+	  if(email.length == 0) {
+		producePrompt('Invalid Email<br>','email-error', 'red');
+		return false;
+
+	}
+
+	if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+
+		producePrompt('Invalid Email<br>', 'email-error', 'red');
+		return false;
+
+	}
+	producePrompt('','email-error','red');
+	return true;
+}
+
+function validateMessage() {
+	var message = document.getElementById('contact-message').value;
+	var required = 30;
+	var left = required - message.length;
+
+	if (left > 0) {
+		producePrompt('Atleast '+left + ' more characters required in message field','message-error','red');
+		return false;
+	}
+	producePrompt('','message-error','red');
+	return true;
+
+}
+
+function validateForm() {
+  if (!validateName() || !validateEmail() || !validateMessage()) {
+    jsShow('submit-error');
+    producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+    return false;
+	}
+	else {
+
+	}
+}
+
+function jsShow(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+function jsHide(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+
+function producePrompt(message, promptLocation, color) {
+  document.getElementById(promptLocation).innerHTML = message;
+  document.getElementById(promptLocation).style.color = color;
+}
